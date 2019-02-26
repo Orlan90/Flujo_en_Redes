10099f44512cd9dc7060f729a5bee75d2cff3c24
Orlan90 <orly230990@gmail.com>
2019-02-26 02:34:18 -0600

    Create grafo_simple_no_dirigido_ciclico.py

---
+++ Tarea_2/grafo_simple_no_dirigido_ciclico.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+GNDC = nx.Graph()
+
+GNDC.add_node("F")
+GNDC.add_nodes_from(["C1","C2","C3","C4"])
+
+GNDC.add_edges_from([("F","C3"),("C3","C4"),("C4","C2"),("C2","C1"),("C1","F")])
+
+nx.draw_random(GNDC, node_size = 2000, node_color = 'y', node_shape = 'o', with_labels=True)
+plt.draw()
+plt.savefig("GNDC.eps")
+plt.show()
